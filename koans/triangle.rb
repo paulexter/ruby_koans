1a69b2e22bbf3d204bd0f148071ea982ea286106
paulexter <paulexter@gmail.com>
2015-09-27 22:11:20 -0400

    progress

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	begin
+		sides = [a,b,c].sort
+		case
+			when sides.select {|x| x <= 0}.length > 1
+				raise TriangleError, "Zero-length or negative side"
+			when sides[-1] >= sides[0..-2].reduce(:+)
+				raise TriangleError, "Hypotenuse too long"
+			when (a == b && b == c)
+				:equilateral
+			when (a == b || b == c || a == c)
+				:isosceles
+			else
+				:scalene
+		end
+	end
 end
 
 # Error class used in part 2.  No need to change this code.
